e713162a32ce2d8646c7eaa2e68aa10901673c1d
snallago7 <nallagorlasaicharan7777@gmail.com>
2025-11-09 17:56:48 -0500

    changed context path and port number to 8080

--- src/main/java/com/Lord_vinayaka/web_mvc_module/controller/HomePageController.java
+++ src/main/java/com/Lord_vinayaka/web_mvc_module/controller/HomePageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomePageController {
 	
-	@GetMapping("/homepage")
+	@GetMapping("/")
 	public String homepageDisplay() {
 		return "homepage";
 	}
